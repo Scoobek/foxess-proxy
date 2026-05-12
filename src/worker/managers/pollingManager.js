bd66eeb5dbf5999663f2181a86716b144b8b363a
Scoobek <Wojciech.skubisz@gmail.com>
2026-05-12 21:38:14 +0200

    Z-42 refactor: polling manager as universal, no only for managinig boiler

--- src/worker/managers/pollingManager.js
+++ src/worker/managers/pollingManager.js
@@ -1,57 +1,41 @@
 /**
- * Polling Manager - zarządzanie interwałowym odpytywaniem API FoxESS
- *
- * Odpowiada za:
- * - Start/stop pollingu
- * - Obsługę błędów w interwale
- * - Aktualizację stanu pollingu
+ * Polling Manager - generyczny menadżer interwałowego odpytywania
  */
 
-import { updateDeviceState } from "../../shared/state.js";
-import { refreshRealtimeData } from "./foxessDataManager.js";
 import { createLogger } from "../../shared/logger.js";
+import { POLLING_INTERVAL_MS } from "../../config/index.js";
 
 const log = createLogger("pollingManager");
-import { calcNextPollAt } from "../../shared/utils/time.js";
-import { POLLING_INTERVAL_MS } from "../../config/index.js";
 
 let pollingInterval = null;
 
 /**
- * Rozpoczyna odpytywanie API
+ * Rozpoczyna odpytywanie
+ * @param {Function} pollFn - funkcja wywoływana co interwał (i natychmiast)
  * @returns {{ success: boolean, alreadyActive?: boolean }}
  */
-export function startPolling() {
+export function startPolling(pollFn) {
     if (pollingInterval) {
         log.debug("Polling już aktywny");
         return { success: false, alreadyActive: true };
     }
 
     log.info("Start polling");
-    updateDeviceState("bojler", {
-        isPolling: true,
-        pollingStartsAt: null,
-        nextPollAt: calcNextPollAt(POLLING_INTERVAL_MS),
-    });
+    pollFn();
 
-    // Natychmiastowe pierwsze odpytanie
-    refreshRealtimeData();
-
-    // Interwał co 5 minut z obsługą błędów
     pollingInterval = setInterval(async () => {
         try {
-            await refreshRealtimeData();
+            await pollFn();
         } catch (error) {
             log.error({ err: error }, "Błąd w polling interval");
         }
-        updateDeviceState("bojler", { nextPollAt: calcNextPollAt(POLLING_INTERVAL_MS) });
     }, POLLING_INTERVAL_MS);
 
     return { success: true };
 }
 
 /**
- * Zatrzymuje odpytywanie API
+ * Zatrzymuje odpytywanie
  * @returns {{ success: boolean, alreadyInactive?: boolean }}
  */
 export function stopPolling() {
@@ -63,19 +47,8 @@ export function stopPolling() {
     log.info("Stop polling");
     clearInterval(pollingInterval);
     pollingInterval = null;
-    updateDeviceState("bojler", {
-        isPolling: false,
-        pollingStopsAt: null,
-        nextPollAt: null,
-    });
 
     return { success: true };
 }
 
-/**
- * Sprawdza czy polling jest aktywny
- * @returns {boolean}
- */
-export function isPollingActive() {
-    return pollingInterval !== null;
-}
+export const isPollingActive = () => pollingInterval !== null;
