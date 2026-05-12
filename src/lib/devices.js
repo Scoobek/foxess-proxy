e9b3f21970bd52709fde397c6396d9069f914d01
Scoobek <Wojciech.skubisz@gmail.com>
2026-05-12 22:12:39 +0200

    Z-42 refactor: common devices file

---
+++ src/lib/devices.js
@@ -0,0 +1,8 @@
+/**
+ * Rejestr urządzeń — jedyne miejsce do edycji przy dodaniu nowego urządzenia
+ */
+
+import * as bojler from "./bojler.js";
+import * as podswietlenieDomu from "./podswietlenieDomu.js";
+
+export const devices = [bojler, podswietlenieDomu];
